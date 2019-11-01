a49c9925dc047e6255ae1fb49a25a1318837e8d4
Oscardj307 <cortezoscar86@gmail.com>
2019-11-01 15:51:13 -0600

    you are the best

--- app/src/main/java/com/example/crudmysqlandroid2/MainActivity.java
+++ app/src/main/java/com/example/crudmysqlandroid2/MainActivity.java
@@ -1,10 +1,36 @@
 package com.example.crudmysqlandroid2;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText et_codigo, et_descripcion, et_precio;
+    private Button btn_guardar, btn_consultaCodigo, btn_consultaDescripcion, btn_eliminar, btn_actualizar;
+
+    boolean inputEt=false;
+    boolean inputEd=false;
+    boolean input1=false;
+    int resultadoInsert=0;
+
+    String senal = "";
+    String codigo = "";
+    String descripcion = "";
+    String precio = "";
+
+    MantenimientoMySQL manto = new MantenimientoMySQL();
+    Dto datos = new Dto();
+
+    //Banderas para saber estados de métodos del CRUD.
+    boolean estadoGuarda = false;
+    boolean estadoEliminar = false;
+
+    AlertDialog.Builder dialogo;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
